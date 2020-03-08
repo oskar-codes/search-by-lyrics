087a5be59ff0b81a1d7bcfbf291025659f9ffebb
Oskar Codes <oskarbertrand@icloud.com>
2020-03-08 18:22:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var resultsContainer = document.querySelector("#results");
-var accessToken;
+var accessToken = "";
+
+document.getElementById("spotify-connect").onclick = function() {
+  window.open(`https://accounts.spotify.com/authorize
+  ?client_id=5fe01282e94241328a84e7c5cc169164
+  &redirect_uri=${encodeURIComponent("https://oskar-codes.github.io/search-by-lyrics/")}
+  &scope=user-read-private%20user-read-email
+  &response_type=token`)
+}
 
 window.onload = function() {
+  
+  if (/#access_token=.+/.test(window.location.href)) {
+    document.getElementById("spotify-connect").style.display = "none";
+    accessToken = window.location.href.match(/(?<=access_token).+?(?=&)/)[0];
+  }
+  
   document.querySelector(".gsc-search-button .gsc-search-button-v2").onclick = function() {
     resultsContainer.innerHTML = "Loading...";
     window.setTimeout(function(){
@@ -20,39 +34,41 @@ window.onload = function() {
 }
 
 function updateResults() {
-  resultsContainer.innerHTML = "";
+  if (accessToken !== "") {
+    resultsContainer.innerHTML = "";
 
-  var results = document.querySelector(".gsc-expansionArea").children;
+    var results = document.querySelector(".gsc-expansionArea").children;
 
-  
-  var r = results[0];
-  
-  var text = "";
-  var allGood = true;
-  try {
-    text = r.children[0].children[0].children[0].children[0].innerHTML.replace(/\|.+/,"").replace(/lyrics/ig, "").replace(/<b>/g,"").replace(/<\/b>/g,"").trim();
-  } catch (e) {
-    console.log(e);
-    resultsContainer.innerHTML = "We couldn't find your song :(";
-    allGood = false;
-  }
 
-  if (allGood) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.spotify.com/v1/search?q=${encodeURIComponent(text)}&type=track`, true);
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Authorization', 'Bearer BQDGyChjIS61DmHLUDYFdwmrzplBgLX8xNtwPLC6WYjGI6l5MweM8-u9QfPvDiZWgBWgOB5iazWvVg2r4wo5vn_kGvzsPb2kqTkKoaE1HYgUWIPEfP1QuKJQPeECS0USoB4lYHZabFiQCH1-OmOQaODsr_gE0JPZ3A');
-    xhr.send();
+    var r = results[0];
 
-    xhr.onload = function(e) {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        try {
-          resultsContainer.innerHTML = getTrackHTML(response.tracks.items[0].uri);
-        } catch (e) {
-          console.log(e);
-          resultsContainer.innerHTML = `Your song seems to be <b>${text}</b>, but we couldn't find it on Spotify :(`;
+    var text = "";
+    var allGood = true;
+    try {
+      text = r.children[0].children[0].children[0].children[0].innerHTML.replace(/\|.+/,"").replace(/lyrics/ig, "").replace(/<b>/g,"").replace(/<\/b>/g,"").trim();
+    } catch (e) {
+      console.log(e);
+      resultsContainer.innerHTML = "We couldn't find your song :(";
+      allGood = false;
+    }
+
+    if (allGood) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', `https://api.spotify.com/v1/search?q=${encodeURIComponent(text)}&type=track`, true);
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Authorization', 'Bearer BQDGyChjIS61DmHLUDYFdwmrzplBgLX8xNtwPLC6WYjGI6l5MweM8-u9QfPvDiZWgBWgOB5iazWvVg2r4wo5vn_kGvzsPb2kqTkKoaE1HYgUWIPEfP1QuKJQPeECS0USoB4lYHZabFiQCH1-OmOQaODsr_gE0JPZ3A');
+      xhr.send();
+
+      xhr.onload = function(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var response = JSON.parse(xhr.responseText);
+          try {
+            resultsContainer.innerHTML = getTrackHTML(response.tracks.items[0].uri);
+          } catch (e) {
+            console.log(e);
+            resultsContainer.innerHTML = `Your song seems to be <b>${text}</b>, but we couldn't find it on Spotify :(`;
+          }
         }
       }
     }

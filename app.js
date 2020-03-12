ff1bfc925808d24eb79fe1751da9be16a4e40d4d
Oskar Codes <oskarbertrand@icloud.com>
2020-03-12 15:14:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 var resultsContainer = document.querySelector("#results");
-var inputField = document.getElementById("gsc-i-id1");
 var accessToken = "";
 var spotify = true;
 
 window.onload = function() {
-  document.querySelector("input").setAttribute("placeholder", "Enter some lyrics");
+  var inputField = document.getElementById("gsc-i-id1");
+  inputField.setAttribute("placeholder", "Enter some lyrics");
 
   if (/#access_token\=.+/.test(location.href)) {
     document.getElementById("connect-container").style.display = "none";
     accessToken = location.href.toString().match(/(?:\#access_token\=).+?(?=\&)/)[0].replace("#access_token=", "");
   }
   document.querySelector(".gsc-search-button .gsc-search-button-v2").onclick = function() {
-    try {
     if (inputField.value !== "" && !!inputField.value.trim()) {
       resultsContainer.innerHTML = "Loading...";
       window.setTimeout(function(){
         updateResults();
       },3000)
     }
-    } catch(e) {alert(e)}
   }
   
   inputField.onkeyup = function(e) {
-    alert(inputField.value + " / " + !!inputField.value.trim())
     if (e.key === "Enter" && inputField.value !== "" && !!inputField.value.trim()) {
       resultsContainer.innerHTML = "Loading...";
       window.setTimeout(function(){
